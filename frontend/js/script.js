df95bc3734395c1e33d28f269d352ea2744cec9c
txb22 <tranxuanbacit@gmail.com>
2021-09-27 17:05:34 +0700

    update

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,6 @@
 $(function () {
     $('.btn_click').click(function () {
-        $('html, body').animate({ scrollTop: $('section.section-3').offset().top }, 'slow');
+        $('html, body').animate({ scrollTop: $('.section-3').offset().top }, 'slow');
         return false;
     });
 });
